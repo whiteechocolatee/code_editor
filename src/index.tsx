774ab53097560d359707f3ee5dc319878619e379
Andrey Chorniy <creatuseandr@icloud.com>
2023-04-22 18:03:08 +0300

    component for displaying bundled code

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,58 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import * as esbuild from 'esbuild-wasm';
+import { useEffect, useState, useRef } from 'react';
+import { unpkgPathPlugin } from './plugins/unpkg-glupin';
 
 const App = () => {
+  const ref = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState<any>();
+
+  const startService = () => {
+    try {
+      ref.current = esbuild.initialize({
+        worker: true,
+        wasmURL: '/esbuild.wasm',
+      });
+    } catch (err) {
+      throw new Error('Something went wrong');
+    }
+  };
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+    const result = await esbuild.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+    });
+    setCode(result.outputFiles[0].text);
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
   return (
     <div>
-      <h1>hi</h1>
+      <textarea
+        value={input}
+        onChange={(e) =>
+          setInput(e.target.value)
+        }></textarea>
+      <div>
+        <button onClick={onClick}>Compile</button>
+        <pre>{code}</pre>
+      </div>
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+
+root.render(<App />);
