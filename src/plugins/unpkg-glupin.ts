596314212a8c89fd380e513fc01db914df9a1263
Andrey Chorniy <creatuseandr@icloud.com>
2023-04-23 15:58:01 +0300

    separate onResolve functions

--- src/plugins/unpkg-glupin.ts
+++ src/plugins/unpkg-glupin.ts
@@ -10,46 +10,33 @@ export const unpkgPathPlugin = (code: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
-      //  This code block is defining a plugin for esbuild that handles resolving of module paths. The
-      // `build.onResolve` function is called whenever esbuild tries to resolve a module path. The function
-      // takes a filter and a callback function. The filter in this case is '/.*/', which matches all
-      // paths. The callback function is an async function that takes an `args` object as its argument. The
-      // function checks if the path is 'index.js' and returns the path and namespace 'a' if it is. If the
-      // path includes './' or '../', it constructs a URL using the path and the resolveDir property of the
-      // args object, and returns the URL and namespace 'a'. Otherwise, it constructs a URL using the path
-      // and returns the URL and namespace 'a'.
+      // handle root entry file index.js
+      build.onResolve({ filter: /(^index\.js$)/ }, () => {
+        return {
+          path: 'index.js',
+          namespace: 'a',
+        };
+      });
+      // handle relative paths
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
+        return {
+          namespace: 'a',
+          path: new URL(
+            args.path,
+            'https://unpkg.com' + args.resolveDir + '/',
+          ).href,
+        };
+      });
+      // handle main file of a module
       build.onResolve(
         { filter: /.*/ },
         async (args: any) => {
-          console.log('onResolve', args);
-          if (args.path === 'index.js') {
-            return { path: args.path, namespace: 'a' };
-          }
-
-          if (
-            args.path.includes('./') ||
-            args.path.includes('../')
-          ) {
-            return {
-              namespace: 'a',
-              path: new URL(
-                args.path,
-                'https://unpkg.com' + args.resolveDir + '/',
-              ).href,
-            };
-          }
-
           return {
             namespace: 'a',
             path: `https://unpkg.com/${args.path}`,
           };
         },
       );
-
-      //  This code block is defining a plugin for esbuild that handles loading of modules. The
-      // `build.onLoad` function is called whenever esbuild tries to load a module. The function takes a
-      // filter and a callback function. The filter in this case is '/.*/', which matches all paths. The
-      // callback function is an async function that takes an `args` object as its argument.
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
         if (args.path === 'index.js') {
