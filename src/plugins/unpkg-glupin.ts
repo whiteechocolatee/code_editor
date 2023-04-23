0b87e7a81401988b10e30561497ed07811b2001d
Andrey Chorniy <creatuseandr@icloud.com>
2023-04-23 15:48:54 +0300

    add props to plugin, and provide user code to build.onLoad

--- src/plugins/unpkg-glupin.ts
+++ src/plugins/unpkg-glupin.ts
@@ -6,7 +6,7 @@ const fileCache = localforage.createInstance({
   name: 'filecache',
 });
 
-export const unpkgPathPlugin = () => {
+export const unpkgPathPlugin = (code: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
@@ -55,10 +55,7 @@ export const unpkgPathPlugin = () => {
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
-            contents: `
-              const message = require('react')
-              console.log(message)
-            `,
+            contents: code,
           };
         }
 
